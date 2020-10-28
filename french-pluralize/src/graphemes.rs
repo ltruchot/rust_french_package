9760e8a314e63849f14a8259d5be2a92291b6282
Loic TRUCHOT <loic.truchot.pro@gmail.com>
2020-10-28 16:30:48 +0100

    feat(master): plural for ail ending words

--- french-pluralize/src/graphemes.rs
+++ french-pluralize/src/graphemes.rs
@@ -1,11 +1,6 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
-use unicode_segmentation::{Graphemes, UnicodeSegmentation};
-
-fn graphemes_iter_len(it: &Graphemes) -> usize {
-    let (_, right) = it.size_hint(); // get Optional length
-    right.unwrap_or(0) // make it a valid usize
-}
+use unicode_segmentation::UnicodeSegmentation;
 
 pub fn take_last(word: &str) -> String {
     let s = String::from(word);
@@ -19,13 +14,17 @@ pub fn take_last(word: &str) -> String {
 
 pub fn take_lasts(word: &str, n: usize) -> String {
     // chars
-    let chars = word.graphemes(true);
+    let chars = word.graphemes(true).collect::<Vec<&str>>();
     // length of chars
-    let len = graphemes_iter_len(&chars);
-
+    let len = chars.len();
     // skip "n" and return this tail
     let to_skip = if len >= n { len - n } else { 0 };
-    chars.skip(to_skip).collect::<Vec<&str>>().join("")
+    chars
+        .iter()
+        .map(AsRef::as_ref)
+        .skip(to_skip)
+        .collect::<Vec<&str>>()
+        .join("")
 }
 
 pub fn drop_last(word: &str) -> String {
@@ -34,22 +33,29 @@ pub fn drop_last(word: &str) -> String {
 
 pub fn drop_lasts(word: &str, n: usize) -> String {
     // chars
-    let chars = word.graphemes(true);
+    let chars = word.graphemes(true).collect::<Vec<&str>>();
     // length of chars
-    let len = graphemes_iter_len(&chars);
+    let len = chars.len();
 
     // take "n" and return this head
     let to_skip = if len >= n { len - n } else { 0 };
-    chars.take(to_skip).collect::<Vec<&str>>().join("")
+    chars
+        .iter()
+        .map(AsRef::as_ref)
+        .take(to_skip)
+        .collect::<Vec<&str>>()
+        .join("")
 }
 
 #[cfg(test)]
+#[allow(clippy::non_ascii_literal)]
 mod tests {
     use super::*;
 
     #[test]
     fn take_last_works() {
         assert_eq!(take_last("tests"), "s");
+        assert_eq!(take_last("xârâcùù"), "ù");
         assert_eq!(take_last("a"), "a");
         assert_eq!(take_last(""), "");
     }
@@ -57,18 +63,22 @@ mod tests {
     #[test]
     fn take_lasts_works() {
         assert_eq!(take_lasts("tests", 0), "");
+        assert_eq!(take_lasts("xârâcùù", 0), "");
         assert_eq!(take_lasts("a", 0), "");
         assert_eq!(take_lasts("", 0), "");
 
         assert_eq!(take_lasts("tests", 1), "s");
+        assert_eq!(take_lasts("xârâcùù", 1), "ù");
         assert_eq!(take_lasts("a", 1), "a");
         assert_eq!(take_lasts("", 1), "");
 
         assert_eq!(take_lasts("tests", 2), "ts");
+        assert_eq!(take_lasts("xârâcùù", 2), "ùù");
         assert_eq!(take_lasts("a", 2), "a");
         assert_eq!(take_lasts("", 2), "");
 
         assert_eq!(take_lasts("constitution", 10), "nstitution");
+        assert_eq!(take_lasts("xârâcùù", 10), "xârâcùù");
         assert_eq!(take_lasts("a", 10), "a");
         assert_eq!(take_lasts("", 10), "");
     }
@@ -76,6 +86,7 @@ mod tests {
     #[test]
     fn drop_last_works() {
         assert_eq!(drop_last("tests"), "test");
+        assert_eq!(drop_last("xârâcùù"), "xârâcù");
         assert_eq!(drop_last("a"), "");
         assert_eq!(drop_last(""), "");
     }
@@ -83,18 +94,22 @@ mod tests {
     #[test]
     fn drop_lasts_works() {
         assert_eq!(drop_lasts("tests", 0), "tests");
+        assert_eq!(drop_lasts("xârâcùù", 0), "xârâcùù");
         assert_eq!(drop_lasts("a", 0), "a");
         assert_eq!(drop_lasts("", 0), "");
 
         assert_eq!(drop_lasts("tests", 1), "test");
+        assert_eq!(drop_lasts("xârâcùù", 1), "xârâcù");
         assert_eq!(drop_lasts("a", 1), "");
         assert_eq!(drop_lasts("", 1), "");
 
         assert_eq!(drop_lasts("tests", 2), "tes");
+        assert_eq!(drop_lasts("xârâcùù", 2), "xârâc");
         assert_eq!(drop_lasts("a", 2), "");
         assert_eq!(drop_lasts("", 2), "");
 
         assert_eq!(drop_lasts("constitution", 10), "co");
+        assert_eq!(drop_lasts("xârâcùù", 10), "");
         assert_eq!(drop_lasts("a", 10), "");
         assert_eq!(drop_lasts("", 10), "");
     }
--- french-pluralize/src/lib.rs
+++ french-pluralize/src/lib.rs
@@ -4,28 +4,36 @@ mod graphemes;
 
 #[must_use]
 pub fn pluralize(s: &str) -> String {
-    // global exception
+    // TODO: disambiguation
+    // "lieu" the place take an "x", when "lieu" the fish take an "s"
+    // "travail" the work take an "x", "travail" the horse tool take an "s"
+
+    // TODO: two possibilities
+    // final -> finals / finaux
+    // ail -> ails / aulx
+
+    // 1) global exceptions
+    if s == "" {
+        return String::new();
+    }
     if s == "oeil" {
         return String::from("yeux");
     }
 
+    // 2) local exceptions
     let result = String::from(s);
-    // ambiguous
-    // lieu (prend un "x" si "endroit", prend un "s" si poisson)
 
-    // les deux possibles
-    // final / finaux
-
-    // ending with s, x, z
+    // -- ending with "s", "x", "z"
     let last = graphemes::take_last(s);
     if last == "s" || last == "x" || last == "z" {
         return result;
     }
 
-    // ending with au, eu, ou
-    let lasts = graphemes::take_lasts(s, 2);
-    if lasts == "au" || lasts == "eu" || lasts == "ou" || lasts == "al" {
-        return match lasts.as_str() {
+    // -- ending with "au", "eu", "ou", "al"
+    let last_2_graphemes = graphemes::take_lasts(s, 2);
+    let last2 = last_2_graphemes.as_str();
+    if last2 == "au" || last2 == "eu" || last2 == "ou" || last2 == "al" {
+        return match last2 {
             // exceptions
             "ou" => match s {
                 "bijou" | "chou" | "genou" | "caillou" | "hibou" | "joujou" | "pou" | "ripou"
@@ -57,20 +65,36 @@ pub fn pluralize(s: &str) -> String {
                 | "tonal"
                 | "val"
                 | "virginal" => result + "s",
-                _ => graphemes::drop_last(&result) + "ux",
+                _ => graphemes::drop_last(s) + "ux",
             },
-            // classic
-            _ => result + "x",
+            // not reachable
+            _ => result,
         };
     }
+
+    // -- ending with "ail"
+    let last_3_graphemes = graphemes::take_lasts(s, 3);
+    let last3 = last_3_graphemes.as_str();
+    if last3 == "ail" {
+        return match s {
+            "bail" | "corail" | "\u{e9}mail" | "gemmail" | "soupirail" | "travail" | "vantail"
+            | "vitrail" | "fermail" => graphemes::drop_lasts(s, 2) + "ux",
+            // not reachable
+            _ => result + "s",
+        };
+    }
+    // 3) no exception: the most classic form
     result + "s"
 }
 
 #[cfg(test)]
+#[allow(clippy::non_ascii_literal)]
 mod tests {
     use super::pluralize;
     #[test]
     fn pluralize_works() {
+        assert_eq!(pluralize(""), "");
+        assert_eq!(pluralize("a"), "as");
         assert_eq!(pluralize("oeil"), "yeux");
         assert_eq!(pluralize("tests"), "tests");
         assert_eq!(pluralize("animaux"), "animaux");
@@ -83,6 +107,10 @@ mod tests {
         assert_eq!(pluralize("matou"), "matous");
         assert_eq!(pluralize("animal"), "animaux");
         assert_eq!(pluralize("festival"), "festivals");
+        assert_eq!(pluralize("corail"), "coraux");
+        assert_eq!(pluralize("émail"), "émaux");
+        assert_eq!(pluralize("chandail"), "chandails");
         assert_eq!(pluralize("voiture"), "voitures");
+        assert_eq!(pluralize("vélo"), "vélos");
     }
 }
