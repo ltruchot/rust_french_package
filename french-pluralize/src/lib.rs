60445bfdef2ee31b0ec01da44659e13fdbc12f46
Loic TRUCHOT <loic.truchot.pro@gmail.com>
2020-10-28 22:34:36 +0100

    feat(master): new plural rules

--- french-pluralize/src/lib.rs
+++ french-pluralize/src/lib.rs
@@ -1,87 +1,90 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
+mod exceptions;
 mod graphemes;
 
 #[must_use]
-pub fn pluralize(s: &str) -> String {
+/**
+ *  @see https://fr.wiktionary.org/wiki/Annexe:Pluriels_irr%C3%A9guliers_en_fran%C3%A7ais
+ */
+pub fn pluralize_word(word: &str) -> String {
+    // TODO: composed words
+    // ex: œil-de-bœuf
+
     // TODO: disambiguation
-    // "lieu" the place take an "x", when "lieu" the fish take an "s"
-    // "travail" the work take an "x", "travail" the horse tool take an "s"
+    // "lieu" the place takes an "x", when "lieu" the fish take an "s"
+    // "travail" the work takes an "x", when "travail" the horse tool take an "s"
+    // "aïeul" the general ancestor pluralize to "aïeux", when "aïeul" direct grand-parent pluralize to "aïeuls"
+    // "œil" the eye pluralize to "yeux", when "œil" the needle's eye pluralize to "œils"
+    // "ciel" the sky pluralize to "cieux", when "ciel" the bed part pluralize to "ciels"
+    // "banal" the ordinary pluralize to "banals", when "banal" the publication pluralize to "banaux"
 
     // TODO: two possibilities
     // final -> finals / finaux
     // ail -> ails / aulx
 
     // 1) global exceptions
-    if s == "" {
+    if word == "" {
         return String::new();
     }
-    if s == "oeil" {
+    if word == "oeil" {
         return String::from("yeux");
+    }
+    if word == "viel" {
+        return String::from("vieux");
+    }
+    if word == "topos" {
+        return String::from("topo\u{ef}"); // topoï
     }
 
     // 2) local exceptions
-    let result = String::from(s);
+    let result = String::from(word);
 
     // -- ending with "s", "x", "z"
-    let last = graphemes::take_last(s);
+    let last = graphemes::take_last(word);
     if last == "s" || last == "x" || last == "z" {
         return result;
     }
 
     // -- ending with "au", "eu", "ou", "al"
-    let last_2_graphemes = graphemes::take_lasts(s, 2);
+    let last_2_graphemes = graphemes::take_lasts(word, 2);
     let last2 = last_2_graphemes.as_str();
-    if last2 == "au" || last2 == "eu" || last2 == "ou" || last2 == "al" {
+
+    if last2 == "au" || last2 == "eu" || last2 == "ou" || last2 == "al" || /* œu */ last2 == "\u{153}u"
+    {
         return match last2 {
             // exceptions
-            "ou" => match s {
-                "bijou" | "chou" | "genou" | "caillou" | "hibou" | "joujou" | "pou" | "ripou"
-                | "chouchou" | "boutchou" => result + "x",
-                _ => result + "s",
-            },
-            "eu" | "au" => match s {
-                "bleu" | "\u{e9}meu" | "pneu" | "enfeu" | "landau" | "sarrau" | "grau" | "unau"
-                | "senau" => result + "s",
-                _ => result + "x",
-            },
-            "al" => match s {
-                "aval"
-                | "bal"
-                | "banal"
-                | "bancal"
-                | "cal"
-                | "carnaval"
-                | "c\u{e9}r\u{e9}monial"
-                | "choral"
-                | "\u{e9}tal"
-                | "fatal"
-                | "festival"
-                | "natal"
-                | "naval"
-                | "pal"
-                | "r\u{e9}cital"
-                | "r\u{e9}gal"
-                | "tonal"
-                | "val"
-                | "virginal" => result + "s",
-                _ => graphemes::drop_last(s) + "ux",
-            },
+            "ou" => {
+                if exceptions::OU.contains(&word) {
+                    result + "x"
+                } else {
+                    result + "s"
+                }
+            }
+            "eu" | "au" | "\u{153}u" => {
+                if exceptions::AU.contains(&word) || exceptions::EU.contains(&word) {
+                    result + "s"
+                } else {
+                    result + "x"
+                }
+            }
+            "al" => {
+                if exceptions::AL.contains(&word) {
+                    result + "s"
+                } else {
+                    graphemes::drop_last(word) + "ux"
+                }
+            }
             // not reachable
             _ => result,
         };
     }
 
     // -- ending with "ail"
-    let last_3_graphemes = graphemes::take_lasts(s, 3);
+    let last_3_graphemes = graphemes::take_lasts(word, 3);
     let last3 = last_3_graphemes.as_str();
-    if last3 == "ail" {
-        return match s {
-            "bail" | "corail" | "\u{e9}mail" | "gemmail" | "soupirail" | "travail" | "vantail"
-            | "vitrail" | "fermail" => graphemes::drop_lasts(s, 2) + "ux",
-            // not reachable
-            _ => result + "s",
-        };
+    if last3 == "ail" && exceptions::AIL.contains(&word) {
+        return graphemes::drop_lasts(word, 2) + "ux";
     }
     // 3) no exception: the most classic form
     result + "s"
@@ -90,27 +93,28 @@ pub fn pluralize(s: &str) -> String {
 #[cfg(test)]
 #[allow(clippy::non_ascii_literal)]
 mod tests {
-    use super::pluralize;
+    use super::pluralize_word;
     #[test]
     fn pluralize_works() {
-        assert_eq!(pluralize(""), "");
-        assert_eq!(pluralize("a"), "as");
-        assert_eq!(pluralize("oeil"), "yeux");
-        assert_eq!(pluralize("tests"), "tests");
-        assert_eq!(pluralize("animaux"), "animaux");
-        assert_eq!(pluralize("nez"), "nez");
-        assert_eq!(pluralize("bleu"), "bleus");
-        assert_eq!(pluralize("vieu"), "vieux");
-        assert_eq!(pluralize("bateau"), "bateaux");
-        assert_eq!(pluralize("landau"), "landaus");
-        assert_eq!(pluralize("bijou"), "bijoux");
-        assert_eq!(pluralize("matou"), "matous");
-        assert_eq!(pluralize("animal"), "animaux");
-        assert_eq!(pluralize("festival"), "festivals");
-        assert_eq!(pluralize("corail"), "coraux");
-        assert_eq!(pluralize("émail"), "émaux");
-        assert_eq!(pluralize("chandail"), "chandails");
-        assert_eq!(pluralize("voiture"), "voitures");
-        assert_eq!(pluralize("vélo"), "vélos");
+        assert_eq!(pluralize_word(""), "");
+        assert_eq!(pluralize_word("a"), "as");
+        assert_eq!(pluralize_word("oeil"), "yeux");
+        assert_eq!(pluralize_word("tests"), "tests");
+        assert_eq!(pluralize_word("animaux"), "animaux");
+        assert_eq!(pluralize_word("nez"), "nez");
+        assert_eq!(pluralize_word("bleu"), "bleus");
+        assert_eq!(pluralize_word("vieu"), "vieux");
+        assert_eq!(pluralize_word("vœu"), "vœux");
+        assert_eq!(pluralize_word("bateau"), "bateaux");
+        assert_eq!(pluralize_word("landau"), "landaus");
+        assert_eq!(pluralize_word("bijou"), "bijoux");
+        assert_eq!(pluralize_word("matou"), "matous");
+        assert_eq!(pluralize_word("animal"), "animaux");
+        assert_eq!(pluralize_word("festival"), "festivals");
+        assert_eq!(pluralize_word("corail"), "coraux");
+        assert_eq!(pluralize_word("émail"), "émaux");
+        assert_eq!(pluralize_word("chandail"), "chandails");
+        assert_eq!(pluralize_word("voiture"), "voitures");
+        assert_eq!(pluralize_word("vélo"), "vélos");
     }
 }
